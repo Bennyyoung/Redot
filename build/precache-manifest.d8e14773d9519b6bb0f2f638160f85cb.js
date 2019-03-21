3e0a19240afa1a0ec41b2980974711f509a944d0
Bennyyoung <benjamineffiong124@yahoo.com>
2019-03-21 17:04:54 +0100

    Initial Commit

---
+++ build/precache-manifest.d8e14773d9519b6bb0f2f638160f85cb.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = [
+  {
+    "revision": "34ae9ba467243fa92763282f2173196a",
+    "url": "/static/media/im6.34ae9ba4.jpg"
+  },
+  {
+    "revision": "778662718b5223f81cc3",
+    "url": "/static/css/main.34eaaecf.chunk.css"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "f7f08875078f3d9291e7e65b88726fcd",
+    "url": "/static/media/im2.f7f08875.jpg"
+  },
+  {
+    "revision": "109e66f2a66c4194a36f",
+    "url": "/static/js/2.109e66f2.chunk.js"
+  },
+  {
+    "revision": "65a627c3ee17bada47b975d6759bbb24",
+    "url": "/static/media/im3.65a627c3.jpg"
+  },
+  {
+    "revision": "deccbe174a328514270d28cc02b7a86b",
+    "url": "/static/media/im4.deccbe17.jpg"
+  },
+  {
+    "revision": "be0da13c4b6dea0ee43bb1198c92621b",
+    "url": "/static/media/im5.be0da13c.jpg"
+  },
+  {
+    "revision": "778662718b5223f81cc3",
+    "url": "/static/js/main.77866271.chunk.js"
+  },
+  {
+    "revision": "91db1ab7d23284f068179c2ae4b56ea8",
+    "url": "/static/media/im7.91db1ab7.jpg"
+  },
+  {
+    "revision": "ddeab050bdda2a08ce5062f20aeebbdd",
+    "url": "/static/media/im8.ddeab050.jpg"
+  },
+  {
+    "revision": "0e5872130dbbae1049c78084327900a8",
+    "url": "/static/media/im9.0e587213.jpg"
+  },
+  {
+    "revision": "bf8a1908c9441f27c25328ca8412541b",
+    "url": "/static/media/im10.bf8a1908.jpg"
+  },
+  {
+    "revision": "9423567015bb1fdc4fda846a5688bb8a",
+    "url": "/static/media/im11.94235670.jpg"
+  },
+  {
+    "revision": "f683d58a944cc3f80662eb54142516b1",
+    "url": "/static/media/im1.f683d58a.jpg"
+  },
+  {
+    "revision": "109e66f2a66c4194a36f",
+    "url": "/static/css/2.25dd7331.chunk.css"
+  },
+  {
+    "revision": "28e240d2b97119226f97335fa19f50d6",
+    "url": "/index.html"
+  }
+];
